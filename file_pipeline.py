3818a8e2b3264cefb2e7aff76fb386292c99fd1f
rickzhang716 <rickzhang7@gmail.com>
2024-11-13 09:59:25 -0800

    migrated to fsevents

--- file_pipeline.py
+++ file_pipeline.py
@@ -2,17 +2,53 @@
 A CI/CD for my resume.
 """
 
-import os
 import os.path
-import time
 from pathlib import Path
+import logging
+import sys
+
+logger = logging.getLogger("")
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+logger.addHandler(handler)
 
 
-file_path = Path(os.getenv("RESUME_PATH"))
-file_path = file_path.expanduser().resolve()
+pythonpath = os.getenv("PYTHON_PATH")
+sys.path.append(pythonpath)
+import re
+
+from fsevents import Observer, Stream
 
 
-while True:
-    if os.path.exists(file_path):
-        os.system(f'mv {os.getenv("RESUME_SRC")} {os.getenv("RESUME_DEST")}') # noqa
-    time.sleep(30)
+pattern = re.compile("Rick Zhang Resume.*pdf$")
+src_directory = Path(os.getenv("RESUME_SRC"))
+src_directory = src_directory.expanduser().resolve()
+
+dest_directory = Path(os.getenv("RESUME_DEST"))
+dest_directory = str(dest_directory.expanduser().resolve())
+
+counter = 0
+def move_file(event):
+    global counter
+    search_result = re.search(pattern, event.name)
+    if search_result is not None:
+        file_name = search_result.group()
+        file_name = file_name.replace(" ", "\\ ")
+        file_name = file_name.replace("(", "\\(")
+        file_name = file_name.replace(")", "\\)")
+        exit_status = os.system(f"mv {src_directory}/{file_name}  {dest_directory}")  # noqa
+        if exit_status == 0:
+            counter += 1
+            logger.info(f"moved {file_name} into {dest_directory}. {counter} manual resume moves saved.")
+
+
+
+logger.info("file pipeline starting...")
+observer = Observer()
+stream = Stream(move_file, str(src_directory), file_events=True)
+observer.schedule(stream)
+logger.info("waiting for file downloads...")
+observer.run()
+
